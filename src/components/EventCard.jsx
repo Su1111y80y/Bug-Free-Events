2a22bd8e7b8a1cad43488471d237dac75e8115ac
Ivan <ivan.kels@gmail.com>
2025-02-13 19:41:51 +0100

    fetch events

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -10,21 +10,13 @@ const EventCard = ({ event }) => {
       className="relative bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-300 hover:scale-105 cursor-pointer hover:shadow-xl"
       onClick={() => navigate(`/event/${event.id}`)}
     >
-      {/* Ribbon for Special Events */}
-      {event.isFeatured && (
-        <div className="absolute top-2 right-2 bg-indigo-600 text-white px-3 py-1 rounded-full text-sm font-semibold z-10">
-          Featured
-        </div>
-      )}
-
       {/* Image with Gradient Overlay */}
       <div className="relative h-56 overflow-hidden">
         <img
-          src={"./src/images/conference.jpg"} // Use dynamic image if available
+          src={"./src/images/conference.jpg"}
           alt={event.title}
           className="w-full h-full object-cover transition-transform duration-300 hover:scale-110"
         />
-        <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent"></div>
       </div>
 
       {/* Event Details */}
